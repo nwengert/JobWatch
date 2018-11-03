29acbfb7899581c33a53254a428cb7c10823c7c9
Nathan Wengert <nate.weng@gmail.com>
2018-11-03 13:59:05 -0600

    works in react

---
+++ openings_react/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  onClick() {
+    const url = [
+    // Craigslist Gigs
+    'https://saltlakecity.craigslist.org/d/gigs/search/ggg',
+    //  LDS Church
+    'https://careers.lds.org/search/public/search.aspx?lang=ENG',
+    //  MX
+    'https://data.mx.com/company#careers',
+    //  RedOlive
+    'https://www.redolive.com/jobs',
+    //  Lucid
+    'https://www.linkedin.com/company/lucidsoftware/jobs/'
+    ]
+    for(let i = 0; i < url.length; i++) {
+      setTimeout(() => window.open(url[i], '_blank'), 2000);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.onClick}>Open All</button>
+      </div>
+    );
+  }
+}
+export default App;
