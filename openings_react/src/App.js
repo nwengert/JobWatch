3324efddafab6e48ad3bc34bfd9c1e6ee3b54eae
Nathan Wengert <nate.weng@gmail.com>
2018-11-28 23:40:12 -0700

    removed onClick

--- openings_react/src/App.js
+++ openings_react/src/App.js
@@ -2,27 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  onClick() {
-    const url = [
-    // Craigslist Gigs
-    'https://saltlakecity.craigslist.org/d/gigs/search/ggg',
-    //  LDS Church
-    'https://careers.lds.org/search/public/search.aspx?lang=ENG',
-    //  MX
-    'https://data.mx.com/company#careers',
-    //  RedOlive
-    'https://www.redolive.com/jobs',
-    //  Lucid
-    'https://www.linkedin.com/company/lucidsoftware/jobs/',
-    //  BambooHR
-    'https://company.bamboohr.com/jobs/',
-    //  USANA
-    'https://www.linkedin.com/company/usana-health-sciences/jobs/'
-    ]
-    for(let i = 0; i < url.length; i++) {
-      window.open(url[i], '_blank')
-    }
-  }
   render() {
     return (
       <div className="App">
