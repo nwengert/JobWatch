d0b939649ad92f0b633adc0f5d3c5ba379877d67
Nathan Wengert <nate.weng@gmail.com>
2018-12-01 13:17:48 -0700

    added dynamic greeting

--- openings_react/src/App.js
+++ openings_react/src/App.js
@@ -9,8 +9,27 @@ import Practice from './components/Practice';
 
 class App extends Component {
   render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+      fontSize: 30
+    }
+
+    if (hours < 12) {
+      timeOfDay = "morning"
+      styles.color = "#ffa500"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+      styles.color = "#7ec0ee"
+    } else {
+      timeOfDay = "night"
+      styles.color = "#800080"
+    }
+
     return (
       <div className="App">
+        <h1 style={styles}> Good {timeOfDay}!</h1>
         <Primeros />
         <Otros />
         <Investments />
